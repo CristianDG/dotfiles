07a21386ec0308e696fd422244f5e6f509bdb211
CristianDG <professional.cristiandg@gmail.com>
2025-05-06 01:42:31 -0300

    changes

--- .config/nvim/plugin/build_menu/init.lua
+++ .config/nvim/plugin/build_menu/init.lua
@@ -1,18 +1,14 @@
 
-
-local build_command = vim.o.makeprg
+local build_command_history = {}
 
 local set_build_command = function(cmd)
-  build_command = cmd
-  vim.o.makeprg = build_command
+  vim.o.makeprg = cmd
 end
-
-vim.keymap.set('n', '<leader>b', ':BuildMenuSet '.. build_command, { desc = "[B]uild command [S]et" })
 
 vim.api.nvim_create_user_command(
   'BuildMenuPrint',
   function()
-    print(build_command)
+    print(vim.o.makeprg)
   end,
   {}
 )
