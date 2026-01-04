3b14e7a92315f17da6f57a81c44152dd6736554a
CristianDG <39601699+CristianDG@users.noreply.github.com>
2026-01-04 09:56:25 -0300

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -96,6 +96,8 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
+vim.o.gdefault = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
